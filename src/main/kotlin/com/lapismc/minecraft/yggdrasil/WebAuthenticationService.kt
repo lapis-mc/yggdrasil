f2e072744bfdd7575079d0ea53786c17ef7b6ad4
Michael Miller <icy.arctic.fox@gmail.com>
2017-07-25 21:01:56 -0600

    Finish setting up rest and auth service

--- src/main/kotlin/com/lapismc/minecraft/yggdrasil/WebAuthenticationService.kt
+++ src/main/kotlin/com/lapismc/minecraft/yggdrasil/WebAuthenticationService.kt
@@ -1,6 +1,7 @@
 package com.lapismc.minecraft.yggdrasil
 
-import com.lapismc.minecraft.yggdrasil.rest.RestService
+import com.lapismc.minecraft.yggdrasil.rest.*
+import com.lapismc.minecraft.yggdrasil.rest.serialization.AuthenticationResponseDeserializer
 import java.util.*
 
 /**
@@ -28,33 +29,47 @@ class WebAuthenticationService(baseUrl: String) : AuthenticationService {
      * @param clientToken Token used to uniquely identify the client.
      * @param agent Game to authenticate for.
      */
-    override fun authenticate(credentials: Credentials, clientToken: UUID, agent: Agent) = TODO()
+    override fun authenticate(credentials: Credentials, clientToken: UUID, agent: Agent) {
+        val request = AuthenticationRequest(credentials, clientToken, agent)
+        restService.post(request, AuthenticationResponseDeserializer())
+    }
 
     /**
      * Renews the access token given during authentication.
      * This allows continued use of the token pair.
      * @param tokenPair Client and access token used during authentication.
      */
-    override fun refresh(tokenPair: TokenPair) = TODO()
+    override fun refresh(tokenPair: TokenPair) {
+        val request = RefreshRequest(tokenPair)
+        restService.post(request, AuthenticationResponseDeserializer())
+    }
 
     /**
      * Checks whether an access token is still usable.
      * @param tokenPair Client and access token used during authentication.
      */
-    override fun validate(tokenPair: TokenPair) = TODO()
+    override fun validate(tokenPair: TokenPair) {
+        val request = ValidateRequest(tokenPair)
+        restService.post(request)
+    }
 
     /**
      * Invalidates all access tokens.
      * Effectively requires all clients using the player's credentials to re-authenticate.
      * @param credentials User credentials.
      */
-    override fun signout(credentials: Credentials) = TODO()
+    override fun signout(credentials: Credentials) {
+        val request = SignoutRequest(credentials)
+        restService.post(request)
+    }
 
     /**
      * Makes an access token unusable.
      * The user must re-authenticate to use the same client.
      * @param tokenPair Client and access token used during authentication.
      */
-    override fun invalidate(tokenPair: TokenPair) = TODO()
-
+    override fun invalidate(tokenPair: TokenPair) {
+        val request = InvalidateRequest(tokenPair)
+        restService.post(request)
+    }
 }
--- src/main/kotlin/com/lapismc/minecraft/yggdrasil/rest/RestService.kt
+++ src/main/kotlin/com/lapismc/minecraft/yggdrasil/rest/RestService.kt
@@ -20,7 +20,7 @@ class RestService(private val baseUrl: String) {
      * @return Result of the request.
      *  If the request was successful, then the result will contain
      *  the deserialized object produced by [responseDeserializer].
-     *  If the request failed, then the result will contain an exception containing the error information.
+     *  If the request failed, then the result will contain an exception with the error information.
      */
     fun <T: Any> post(request: Request, responseDeserializer: ResponseDeserializable<T>): Result<T, Exception> {
         val url  = baseUrl + request.endpoint
@@ -30,6 +30,32 @@ class RestService(private val baseUrl: String) {
         result.fold(success = {
             // Return expected object type.
             return result
+        }, failure = {
+            // Error occurred, response data contains error.
+            // Deserialize the error into something we can use.
+            val error = ErrorResponseDeserializer().deserialize(response.dataStream)
+            return Result.error(error.toException())
+        })
+    }
+
+    /**
+     * Posts a request to the REST API and checks if there was an error.
+     * This method assumes that the response will be empty upon success.
+     * If an error occurs, then a failure result is returned and an exception is created with the error information.
+     * @param request Request to send to the API.
+     * @return Result of the request.
+     *  If the request was successful, then the result will contain the value true and no error.
+     *  If the request failed, then the result will contain an exception with the error information.
+     */
+    fun post(request: Request): Result<Boolean, Exception> {
+        // TODO: Remove duplicate code.
+        val url  = baseUrl + request.endpoint
+        val body = request.serialize(JsonSerializer())
+        val httpRequest = Fuel.post(url).header(Pair("Content-Type", "application/json")).body(body)
+        val (_, response, result) = httpRequest.response()
+        result.fold(success = {
+            // Return expected object type.
+            return Result.of(true)
         }, failure = {
             // Error occurred, response data contains error.
             // Deserialize the error into something we can use.
