d50ffaa64037089fc4961d51a27f16031eb2062e
Michael Miller <icy.arctic.fox@gmail.com>
2017-07-27 10:44:07 -0600

    Setup return values and document them

--- src/main/kotlin/com/lapismc/minecraft/yggdrasil/AuthenticationService.kt
+++ src/main/kotlin/com/lapismc/minecraft/yggdrasil/AuthenticationService.kt
@@ -1,5 +1,7 @@
 package com.lapismc.minecraft.yggdrasil
 
+import com.github.kittinunf.result.Result
+import com.lapismc.minecraft.yggdrasil.rest.AuthenticationResponse
 import java.util.UUID
 
 /**
@@ -11,33 +13,48 @@ interface AuthenticationService {
      * @param credentials User credentials for authentication.
      * @param clientToken Token used to uniquely identify the client.
      * @param agent Game to authenticate for.
+     * @return Result of the authentication.
+     *  Upon success, an [AuthenticationResponse] is returned.
+     *  If an error occurred, exception information about the problem is returned.
      */
-    fun authenticate(credentials: Credentials, clientToken: UUID, agent: Agent = Agent.minecraft())
+    fun authenticate(credentials: Credentials, clientToken: UUID, agent: Agent = Agent.minecraft()): Result<AuthenticationResponse, Exception>
 
     /**
      * Renews the access token given during authentication.
      * This allows continued use of the token pair.
      * @param tokenPair Client and access token used during authentication.
+     * @return Result of the refresh.
+     *  Upon success, an [AuthenticationResponse] is returned.
+     *  If an error occurred, exception information about the problem is returned.
      */
-    fun refresh(tokenPair: TokenPair)
+    fun refresh(tokenPair: TokenPair): Result<AuthenticationResponse, Exception>
 
     /**
      * Checks whether an access token is still usable.
      * @param tokenPair Client and access token used during authentication.
+     * @return Result of the validation.
+     *  Upon success, a boolean is returned indicating whether the token pair is valid.
+     *  If an error occurred, exception information about the problem is returned.
      */
-    fun validate(tokenPair: TokenPair)
+    fun validate(tokenPair: TokenPair): Result<Boolean, Exception>
 
     /**
      * Invalidates all access tokens.
      * Effectively requires all clients using the player's credentials to re-authenticate.
      * @param credentials User credentials.
+     * @return Result of the signout.
+     *  Upon success, a boolean is returned indicating whether the user was signed out.
+     *  If an error occurred, exception information about the problem is returned.
      */
-    fun signout(credentials: Credentials)
+    fun signout(credentials: Credentials): Result<Boolean, Exception>
 
     /**
      * Makes an access token unusable.
      * The user must re-authenticate to use the same client.
      * @param tokenPair Client and access token used during authentication.
+     * @return Result of the invalidation.
+     *  Upon success, a boolean is returned indicating whether the token pair was invalidated.
+     *  If an error occurred, exception information about the problem is returned.
      */
-    fun invalidate(tokenPair: TokenPair)
+    fun invalidate(tokenPair: TokenPair): Result<Boolean, Exception>
 }
--- src/main/kotlin/com/lapismc/minecraft/yggdrasil/WebAuthenticationService.kt
+++ src/main/kotlin/com/lapismc/minecraft/yggdrasil/WebAuthenticationService.kt
@@ -1,5 +1,6 @@
 package com.lapismc.minecraft.yggdrasil
 
+import com.github.kittinunf.result.Result
 import com.lapismc.minecraft.yggdrasil.rest.*
 import com.lapismc.minecraft.yggdrasil.rest.serialization.AuthenticationResponseDeserializer
 import java.util.*
@@ -28,48 +29,63 @@ class WebAuthenticationService(baseUrl: String) : AuthenticationService {
      * @param credentials User credentials for authentication.
      * @param clientToken Token used to uniquely identify the client.
      * @param agent Game to authenticate for.
+     * @return Result of the authentication.
+     *  Upon success, an [AuthenticationResponse] is returned.
+     *  If an error occurred, exception information about the problem is returned.
      */
-    override fun authenticate(credentials: Credentials, clientToken: UUID, agent: Agent) {
+    override fun authenticate(credentials: Credentials, clientToken: UUID, agent: Agent): Result<AuthenticationResponse, Exception> {
         val request = AuthenticationRequest(credentials, clientToken, agent)
-        restService.post(request, AuthenticationResponseDeserializer())
+        return restService.post(request, AuthenticationResponseDeserializer())
     }
 
     /**
      * Renews the access token given during authentication.
      * This allows continued use of the token pair.
      * @param tokenPair Client and access token used during authentication.
+     * @return Result of the refresh.
+     *  Upon success, an [AuthenticationResponse] is returned.
+     *  If an error occurred, exception information about the problem is returned.
      */
-    override fun refresh(tokenPair: TokenPair) {
+    override fun refresh(tokenPair: TokenPair): Result<AuthenticationResponse, Exception> {
         val request = RefreshRequest(tokenPair)
-        restService.post(request, AuthenticationResponseDeserializer())
+        return restService.post(request, AuthenticationResponseDeserializer())
     }
 
     /**
      * Checks whether an access token is still usable.
      * @param tokenPair Client and access token used during authentication.
+     * @return Result of the validation.
+     *  Upon success, a boolean is returned indicating whether the token pair is valid.
+     *  If an error occurred, exception information about the problem is returned.
      */
-    override fun validate(tokenPair: TokenPair) {
+    override fun validate(tokenPair: TokenPair): Result<Boolean, Exception> {
         val request = ValidateRequest(tokenPair)
-        restService.post(request)
+        return restService.post(request)
     }
 
     /**
      * Invalidates all access tokens.
      * Effectively requires all clients using the player's credentials to re-authenticate.
      * @param credentials User credentials.
+     * @return Result of the signout.
+     *  Upon success, a boolean is returned indicating whether the user was signed out.
+     *  If an error occurred, exception information about the problem is returned.
      */
-    override fun signout(credentials: Credentials) {
+    override fun signout(credentials: Credentials): Result<Boolean, Exception> {
         val request = SignoutRequest(credentials)
-        restService.post(request)
+        return restService.post(request)
     }
 
     /**
      * Makes an access token unusable.
      * The user must re-authenticate to use the same client.
      * @param tokenPair Client and access token used during authentication.
+     * @return Result of the invalidation.
+     *  Upon success, a boolean is returned indicating whether the token pair was invalidated.
+     *  If an error occurred, exception information about the problem is returned.
      */
-    override fun invalidate(tokenPair: TokenPair) {
+    override fun invalidate(tokenPair: TokenPair): Result<Boolean, Exception> {
         val request = InvalidateRequest(tokenPair)
-        restService.post(request)
+        return restService.post(request)
     }
 }
