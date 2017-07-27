486ff791a8cd865fb08389898918212fb0960d32
Michael Miller <icy.arctic.fox@gmail.com>
2017-07-27 12:51:19 -0600

    Implement conversion of error response to exception

--- src/main/kotlin/com/lapismc/minecraft/yggdrasil/rest/ErrorResponse.kt
+++ src/main/kotlin/com/lapismc/minecraft/yggdrasil/rest/ErrorResponse.kt
@@ -1,5 +1,7 @@
 package com.lapismc.minecraft.yggdrasil.rest
 
+import com.lapismc.minecraft.yggdrasil.ForbiddenOperationException
+
 /**
  * Response received for all possible errors.
  * @param error Error name and type.
@@ -16,5 +18,25 @@ class ErrorResponse(val error: String, val message: String, val cause: String? =
      * Converts the error to a logical exception.
      * @return Exception containing the error information.
      */
-    fun toException(): Exception = TODO()
+    fun toException(): Exception {
+        val klass = errorNameToExceptionClass(error)
+        return if(cause == null) {
+            klass.getConstructor(String::class.java).newInstance(message)
+        } else {
+            val causedBy = errorNameToExceptionClass(cause).newInstance()
+            klass.getConstructor(String::class.java, Exception::class.java).newInstance(message, causedBy)
+        }
+    }
+
+    /**
+     * Retrieves the class for the corresponding error string.
+     * @param error Error type.
+     * @return Exception class type.
+     */
+    private fun errorNameToExceptionClass(error: String) = when(error) {
+        // TODO: Is it possible to use the Kotlin class instead of the Java class?
+        "ForbiddenOperationException" -> ForbiddenOperationException::class.java
+        "IllegalArgumentException"    -> IllegalArgumentException::class.java
+        else -> Exception::class.java
+    }
 }
